ef84a0f3265299438b67e85959982097dbb3239a
Zabdiel-Rosas <zabdiel_rosas@outlook.com>
2023-04-12 11:35:40 -0600

    refactor(App): code is refactored into a component to separate the logic

---
+++ src/components/UserList.jsx
@@ -0,0 +1,32 @@
+import { useFetch } from '../hooks/useFetch'
+import '../App.css'
+
+const UserList = () => {
+  const { data, loading, error, handleCancelRequest } = useFetch(
+    import.meta.env.VITE_API_URL
+  )
+
+  return (
+    <>
+      {loading && (
+        <>
+          <button className='button' onClick={handleCancelRequest}>
+            Cancel Request
+          </button>
+          <div>Loading...</div>
+        </>
+      )}
+      {error && <div>Error: {error}</div>}
+      {data && (
+        <ul className='card'>
+          <h3>Users:</h3>
+          {data?.map((user) => {
+            return <li key={user.id}>{user.name}</li>
+          })}
+        </ul>
+      )}
+    </>
+  )
+}
+
+export default UserList
