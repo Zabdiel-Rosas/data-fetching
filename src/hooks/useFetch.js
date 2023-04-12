34e86cd04afa0690cd9c0da6b36b2b5e013e0e43
Zabdiel-Rosas <zabdiel_rosas@outlook.com>
2023-04-12 11:32:49 -0600

    feat(useFetch): abort controller is added to cancel the request when needed

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,23 +4,33 @@ export function useFetch(url) {
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  const [controller, setController] = useState(null)
 
   useEffect(() => {
-    fetch(url)
-      .then((response) => {
-        if (response.status === 200) {
-          return response.json()
-        } else if (response.status === 404) {
-          setError({
-            code: response.status,
-            msg: `Oops!, We couldn't find the resource :(`,
-          })
+    const abortController = new AbortController()
+    setController(abortController)
+
+    fetch(url, { signal: abortController.signal })
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => {
+        if (error.name === 'AbortError') {
+          console.log('Request Canceled catch')
+        } else {
+          setError(error)
         }
       })
-      .then((data) => data && setData(data))
-      .catch((error) => setError(error))
       .finally(() => setLoading(false))
+
+    return () => abortController.abort()
   }, [])
 
-  return { data, loading, error }
+  const handleCancelRequest = () => {
+    if (controller) {
+      controller.abort()
+      setError('Request Canceled')
+    }
+  }
+
+  return { data, loading, error, handleCancelRequest }
 }
